58a8c769a3c458dc30fab7ff771db90359a4afcf
黄树鹏 <825844231@qq.com>
2019-07-23 10:41:30 +0800

    增加框架 增加服务启动

--- Package.swift
+++ Package.swift
@@ -8,13 +8,27 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        //HTTPServer
+        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
+        
+        //        //Request请求日志过滤器
+        //        .package(url: "https://github.com/dabfleming/Perfect-RequestLogger.git",
+        //                 from: "3.0.0"),
+        //
+        //        //将日志写入指定文件
+        .package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git",
+                 from: "3.0.0"),
+        
+        //MySql数据库依赖包
+        .package(url: "https://github.com/PerfectlySoft/Perfect-MySQL.git", from: "3.0.0"),
+        .package(url: "https://github.com/SwiftORM/StORM.git", from: "3.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ServerProject",
-            dependencies: []),
+            dependencies: ["PerfectHTTPServer","PerfectLogger","PerfectMySQL","StORM"]),
         .testTarget(
             name: "ServerProjectTests",
             dependencies: ["ServerProject"]),
--- Sources/ServerProject/main.swift
+++ Sources/ServerProject/main.swift
@@ -1,1 +1,29 @@
-print("Hello, world!")
+import Foundation
+import PerfectLib
+import PerfectHTTP
+import PerfectHTTPServer
+import PerfectCrypto
+
+func handler(request: HTTPRequest, response: HTTPResponse) {
+    // Respond with a simple message.
+    response.setHeader(.contentType, value: "text/html")
+    response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>")
+    // Ensure that response.completed() is called when your processing is done.
+    response.completed()
+}
+var  routes = Routes()
+routes.add(method: .get, uri: "/", handler: handler)
+routes.add(method: .get, uri: "/**",
+           handler: StaticFileHandler(documentRoot: "./webroot", allowResponseFilters: true).handleRequest)
+try HTTPServer.launch(name: "localhost",
+                      port: 8181,
+                      routes: routes,
+                      responseFilters: [
+                        (PerfectHTTPServer.HTTPFilter.contentCompression(data: [:]), HTTPFilterPriority.high)])
+
+
+
+
+ 
+
+
